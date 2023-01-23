e122ac753fbea63d3a0b1352321981a2e1b5d09a
Brayan M. Beltre <brayanmbeltre@gmail.com>
2023-01-23 17:03:15 -0400

    feat: init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
+
+import Vercel from '~/svg/Vercel.svg';
 
 /**
  * SVGR Support
@@ -14,11 +15,62 @@ import Seo from '@/components/Seo';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
+
+const data = {
+  id: 1,
+  image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+  name: 'Brayan Beltre',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ullam amet consectetur nobis distinctio deserunt officia illo officiis repellat corrupti quis dolorem eligendi nostrum, iste tenetur, maxime, sit repellendus magnam.',
+  links: [
+    {
+      id: 1,
+      name: 'Facebook',
+      link: 'http://facebook.com',
+      image: undefined,
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com',
+      image: 'https://avatars.githubusercontent.com/u/51127598?v=4',
+    },
+  ],
+};
 
 export default function HomePage() {
   return (
@@ -26,45 +78,42 @@ export default function HomePage() {
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
 
-      <main>
-        <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
+      <div className='layout max-w-sm'>
+        <nav className='fixed top-4 z-10 w-full max-w-sm'>
+          <div className='flex justify-end rounded-md bg-blue-300/30 p-2 backdrop-blur-sm'>
+            <Vercel className='text-4xl' />
+          </div>
+        </nav>
+      </div>
 
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
+      <main className='mt-8'>
+        <section className=''>
+          <div className='layout relative flex min-h-screen max-w-sm flex-col items-center justify-center py-12 text-center'>
+            <Image
+              src={data.image}
+              height={200}
+              width={200}
+              alt='Profile image'
+            />
 
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
+            <h1 className='mt-4'>{data.name}</h1>
+
+            <p className='mt-2 text-sm text-gray-800'>{data.description}</p>
+
+            {data.links.map((link) => (
+              <ButtonLink
+                key={link.id}
+                className='mt-6 w-full'
+                href={link.link}
+                variant='light'
+              >
+                {link.name}
+              </ButtonLink>
+            ))}
 
             <footer className='absolute bottom-2 text-gray-700'>
               © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
+              <UnderlineLink href='https://linktr.ee'>Linktree</UnderlineLink>
             </footer>
           </div>
         </section>
